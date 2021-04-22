c6f11e42d72377a4937a38846e344a2409ae6f43
Wendy Wu <wwa86@sfu.ca>
2021-04-22 00:04:39 -0700

    implement inversion distance

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,66 +1,105 @@
-# a2.py
-
 from search import *
-import random as r
 import time
+import math
 
-goal_state=(0,1,2,3,4,5,6,7,8)
+goal_state = (0, 1, 2, 3, 4, 5, 6, 7, 8)
+
+
 def make_rand_8puzzle():
     while True:
-        temp = tuple(random.sample((0,1,2,3,4,5,6,7,8), 9))
-        puzzle = EightPuzzle(temp,goal_state)
-        if(puzzle.check_solvability(temp)):
+        temp = tuple(random.sample((0, 1, 2, 3, 4, 5, 6, 7, 8), 9))
+        new_puzzle = EightPuzzle(temp, goal_state)
+        if new_puzzle.check_solvability(temp):
             print("State: ")
             display(temp)
-            return puzzle
+            return new_puzzle
 
 
 # temp = tuple(random.sample((0,1,2,3,4,5,6,7,8), 9))
 def display(state):
     cnt = 0
     for num in state:
-        if (num == 0):
+        if num == 0:
             print("*", end=" ")
         else:
             print(num, end=" ")
         cnt += 1
-        if(cnt%3==0):
+        if cnt % 3 == 0:
             print(end="\n")
     print()
-###Heuristics 
+
+
+###Heuristics
 
 ##taken from textbook code
 def misplaced(node):
     return sum(s != g for (s, g) in zip(node.state, goal_state))
 
+
 ###taken from textbook
 def manhattan(node):
     state = node.state
-    index_goal = {0:[0,0], 1:[0,1], 2:[0,2], 3:[1,0], 4:[1,1], 5:[1,2], 6:[2,0], 7:[2,1], 8:[2,2]}
+    index_goal = {0: [0, 0], 1: [0, 1], 2: [0, 2], 3: [1, 0], 4: [1, 1], 5: [1, 2], 6: [2, 0], 7: [2, 1], 8: [2, 2]}
     index_state = {}
-    index = [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]]
+    index = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
     x, y = 0, 0
-    
+
     for i in range(len(state)):
         index_state[state[i]] = index[i]
-    
+
     mhd = 0
-    
-    #modified to not consider blank tile
-    for i in range(1,9):
+
+    # modified to not consider blank tile
+    for i in range(1, 9):
         for j in range(2):
             mhd += abs(index_goal[i][j] - index_state[i][j])
-    
+
     return mhd
 
 
+def inversion(node):
+    node = node.state
+    # left-to-right, top-to-bottom fashion
+    vertical_inversions = 0
+    for i in range(9):
+        if node[i] == 0:
+            continue
+        for j in range(i + 1, 9):
+            if node[j] == 0:
+                continue
+            if node[j] < node[i]:
+                vertical_inversions += 1
+    vertical_lowerbound = math.floor(vertical_inversions / 3) + vertical_inversions % 3
+
+    # top-to-bottom, left-to-right fashion
+    new_order = [0, 3, 6, 1, 4, 7, 2, 5, 8]
+    new_node = []
+    for i in new_order:
+        new_node.append(node[i])
+    horizontal_inversions = 0
+    for i in range(9):
+        if new_node[i] == 0:
+            continue
+        for j in range(i + 1, 9):
+            if new_node[j] == 0:
+                continue
+            if new_node[j] < new_node[i]:
+                horizontal_inversions += 1
+    horizontal_lowerbound = math.floor(horizontal_inversions / 3) + horizontal_inversions % 3
+
+    # add horizontal lowerbound and vertical lowerbound
+    total_inversion_distance = math.floor(horizontal_lowerbound + vertical_lowerbound)
+
+    # TODO calculate change in version by using skipped row/column instead of recalculating the entire board
+    return total_inversion_distance
 
 
 ##taken from textbook code
 def max_heuristic(node):
-    mis_score=misplaced(node)
-    man_score=manhattan(node)
-    return max(mis_score,man_score)
+    mis_score = misplaced(node)
+    man_score = manhattan(node)
+    # inv_score = inversion(node)
+    return max(mis_score, man_score)
 
 
 ###Modifed Textbook code
@@ -81,7 +120,7 @@ def best_first_graph_search(problem, f, display=False):
         node = frontier.pop()
         if problem.goal_test(node.state):
             if display:
-                print("Nodes expanded: ",len(explored))
+                print("Nodes expanded: ", len(explored))
             return node
         explored.add(node.state)
         for child in node.expand(problem):
@@ -93,6 +132,7 @@ def best_first_graph_search(problem, f, display=False):
                     frontier.append(child)
     return None
 
+
 def astar_search(problem, h=None, display=True):
     """A* search is best-first graph search with f(n) = g(n)+h(n).
     You need to specify the h function when you call astar_search, or
@@ -103,17 +143,15 @@ def astar_search(problem, h=None, display=True):
 
 ############################################# driver code
 
-puzzle=make_rand_8puzzle()
-
-
+puzzle = make_rand_8puzzle()
 
 ##misplaced-tiles
 print("A* with misplaced-tiles heuristic:")
 start_time = time.time()
 
-sol=astar_search(puzzle,"",True).solution()
-print("Solution: ",sol)
-print("Solution length: ",len(sol))
+sol = astar_search(puzzle, "", True).solution()
+print("Solution: ", sol)
+print("Solution length: ", len(sol))
 
 elapsed_time = time.time() - start_time
 print(f'elapsed time (in seconds): {elapsed_time}s')
@@ -122,10 +160,21 @@ print(f'elapsed time (in seconds): {elapsed_time}s')
 print("\n\nA* with manhattan heuristic:")
 start_time = time.time()
 
-print(astar_search(puzzle,manhattan,True).state)
-sol=astar_search(puzzle,manhattan,True).solution()
-print("Solution: ",sol)
-print("Solution length: ",len(sol))
+# print(astar_search(puzzle,manhattan,True).state)
+sol = astar_search(puzzle, manhattan, True).solution()
+print("Solution: ", sol)
+print("Solution length: ", len(sol))
+
+elapsed_time = time.time() - start_time
+print(f'elapsed time (in seconds): {elapsed_time}s')
+
+## inversion
+print("\n\nA* with inversion-distance heuristic:")
+start_time = time.time()
+
+sol = astar_search(puzzle, inversion, True).solution()
+print("Solution: ", sol)
+print("Solution length: ", len(sol))
 
 elapsed_time = time.time() - start_time
 print(f'elapsed time (in seconds): {elapsed_time}s')
@@ -134,9 +183,9 @@ print(f'elapsed time (in seconds): {elapsed_time}s')
 print("\n\nA* with max-misplaced-manhattan heuristic:")
 start_time = time.time()
 
-sol=astar_search(puzzle,max_heuristic,True).solution()
-print("Solution: ",sol)
-print("Solution length: ",len(sol))
+sol = astar_search(puzzle, max_heuristic, True).solution()
+print("Solution: ", sol)
+print("Solution length: ", len(sol))
 
 elapsed_time = time.time() - start_time
 print(f'elapsed time (in seconds): {elapsed_time}s')
