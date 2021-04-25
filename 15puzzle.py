5471e52024093f5ec5885316f79c7e196be98f2f
Wendy Wu <wwa86@sfu.ca>
2021-04-25 03:43:34 -0700

    add sample results

--- 15puzzle.py
+++ 15puzzle.py
@@ -1,9 +1,8 @@
-import csv
-
 from Puzzle import FifteenPuzzle
 from search import *
 import time
 import math
+import csv
 
 N = 4
 goal_state = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0)
